fbe16fcc472e669c9fb5f7f574a0185f4b90aa46
mshuranskiy <70566243+mshuranskiy@users.noreply.github.com>
2020-09-16 17:50:45 +0300

    Создание функций сичтывания и записи в файл информации о трубах и компрессорных стациях

--- laba0Shuranskiy/main.cpp
+++ laba0Shuranskiy/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 struct Truba//Труба
 {
@@ -42,17 +43,17 @@ KS creatKS()//Создание компрессорной станции
 void printTruba(Truba t)//Вывод информации о трубе
 {
 	cout << "Данные о трубе:\n";
-	cout << "Длина трубы " << t.dlina<< endl;
-	cout << "Диаметр турбы " << t.diametr << endl;
-	cout << "Состояние трубы " << (t.sostoyanie ? "В ремонте\n" : "Не в ремонте\n");
+	cout << "Длина трубы: " << t.dlina<< endl;
+	cout << "Диаметр турбы: " << t.diametr << endl;
+	cout << "Состояние трубы: " << (t.sostoyanie ? "В ремонте\n" : "Не в ремонте\n");
 }
 void printKS(KS ks)//Вывод информации о компрессорной станции
 {
 	cout << "Данные о компрессорной станции:\n";
-	cout << "Имя компрессорной станции " << ks.name;
-	cout << "Количество цехов на компрессорной станции " << ks.n;
-	cout << "Количество цехов в работе " << ks.ninwork;
-	cout << "Эффективность компрессорной станции " << ks.efect;
+	cout << "Имя компрессорной станции: " << ks.name << endl;
+	cout << "Количество цехов на компрессорной станции: " << ks.n << endl;
+	cout << "Количество цехов в работе: " << ks.ninwork << endl;
+	cout << "Эффективность компрессорной станции: " << ks.efect << endl;
 }
 void editTruba(Truba &t)//Отправить трубу в ремонт
 {
@@ -66,10 +67,69 @@ void editKS(KS &ks)//Изменение цоехов в работе
 	ks.ninwork = i;
 
 }
+void savefileTruba(Truba t)//Сохранение информации о трубе
+{
+	ofstream fout;
+	fout.open("info.txt", ios::out);
+	if (fout.is_open())
+	{
+		fout << t.dlina << endl;
+		fout << t.diametr << endl;
+		fout << t.sostoyanie << endl;
+		fout.close();
+	}
+}
+Truba inputfileTruba()//Считывание информации о трубе
+{
+	Truba t;
+	ifstream fin;
+	fin.open("info.txt", ios::in);
+	if (fin.is_open())
+	{
+		fin >> t.dlina;
+		fin >> t.diametr;
+		fin >> t.sostoyanie;
+		fin.close();
+	}
+	return t;
+}
+void savefileKS(KS ks)//Сохранение информации о компрессорной станции
+{
+	ofstream fout;
+	fout.open("info.txt", ios::out);
+	if (fout.is_open())
+	{
+		fout << ks.name<<endl;
+		fout  << ks.n << endl;
+		fout  << ks.ninwork << endl;
+		fout << ks.efect << endl;
+		fout.close();
+	}
+}
+KS inputfileKS()//Считывание информации о компрессорной станции
+{
+	KS ks;
+	ifstream fin;
+	fin.open("info.txt", ios::in);
+	if (fin.is_open())
+	{
+		fin>> ks.name;
+		fin >> ks.n;
+		fin >> ks.ninwork;
+		fin >> ks.efect;
+		fin.close();
+	}
+	return ks;
+}
 int main()
 {
 	setlocale(LC_ALL, "Russian");
-	Truba infoTruba=creatTruba();
+	KS infoks = creatKS();
+	editKS(infoks);
+	printKS(infoks);
+	savefileKS(infoks);
+	/*Truba infoTruba=inputfileTruba();
 	editTruba(infoTruba);
 	printTruba(infoTruba);
+	savefileTruba(infoTruba);*/
 }
