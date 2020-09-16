dfcb25ed7eb6d7c812f19fda6e709b805e046a8f
mshuranskiy <70566243+mshuranskiy@users.noreply.github.com>
2020-09-16 16:26:32 +0300

    Создание функций  задания данных о трубе и компрессорной станции и функций вывода в консоль информации о трубе и компрессорной станции

--- laba0Shuranskiy/main.cpp
+++ laba0Shuranskiy/main.cpp
@@ -1,22 +1,62 @@
 #include <iostream>
 using namespace std;
-struct Truba
+struct Truba//Труба
 {
 	string id;//Идентификатор
 	float dlina;//Длина
 	int diametr;//Диаметр
-	bool sostoyanie;//Состояние в работе или нет
+	bool sostoyanie;//Состояние в ремонте или нет
 
 };
-struct KS
+struct KS//Компрессорная станция
 {
 	string name;//Имя
 	string id;//идентификатор
-	int n;//Количество
-	int ninwork;//Количсевто в работе
+	int n;//Количество цехов
+	int ninwork;//Количсевто цехов в работе
 	int efect;//Эффективность
 };
+Truba creatTruba()//Создание трубы
+{
+	Truba t;
+	cout << "Введите длину трубы\n";
+	cin >> t.dlina;
+	cout << "Введите диаметр трубы\n";
+	cin >> t.diametr;
+	t.sostoyanie = false;
+	return t;
+}
+KS creatKS()//Создание компрессорной станции
+{
+	KS ks;
+	cout << "Введите имя компрессорной станции\n";
+	cin >> ks.name;
+	cout << "Введите количество цехов\n";
+	cin >> ks.n;
+	cout << "Введите количество цехов в работе\n";
+	cin >> ks.ninwork;
+	cout << "Введите эффективность компрессорной станцции\n";
+	cin >> ks.efect;
+	return ks;
+}
+void printTruba(Truba t)//Вывод информации о трубе
+{
+	cout << "Данные о трубе:\n";
+	cout << "Длина трубы " << t.dlina<< endl;
+	cout << "Диаметр турбы " << t.diametr << endl;
+	cout << "Состояние трубы " << (t.sostoyanie ? "В ремонте\n" : "Не в ремонте\n");
+}
+void printKS(KS ks)//Вывод информации о компрессорной станции
+{
+	cout << "Данные о компрессорной станции:\n";
+	cout << "Имя компрессорной станции " << ks.name;
+	cout << "Количество цехов на компрессорной станции " << ks.n;
+	cout << "Количество цехов в работе " << ks.ninwork;
+	cout << "Эффективность компрессорной станции " << ks.efect;
+}
 int main()
 {
-	
+	setlocale(LC_ALL, "Russian");
+	Truba infoTruba=creatTruba();
+	printTruba(infoTruba);
 }
